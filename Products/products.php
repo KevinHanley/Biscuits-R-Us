129b5709851813c862785c7d24a3c2030df69c13
Kevin Hanley <kvnhanley@gmail.com>
2019-10-20 16:20:46 +0100

    added products page and containers to home page

---
+++ Products/products.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Biscuits R' Us - Products</title>
+
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+	<!-- style sheets -->
+	<link rel="stylesheet" href="../Navigation/navbar.css" type="text/css"/>
+	<link rel="stylesheet" href="./products.css" type="text/css"/>
+</head>
+
+<body>
+	<!-- this php will add the navbar -->
+	<?php include("../Navigation/navbar.html"); ?>
+
+	<!-- Page header -->
+	<div class="jumbotron text-center">
+	  <h1>Prodcuts Here!!</h1> 
+	  <p>This is the spot alright!</p> 
+	</div>
+
+
+</body>
+</html>
--- home.php
+++ home.php
@@ -31,6 +31,7 @@
 	</div>
 	
 
+
 	<!-- Image Carousel -->
 	<div class="container">
 	  <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -47,16 +48,28 @@
 	    	<!-- Photo by Cayla1 on Unsplash -->
 	      <div class="item active">
 	        <img src="/Images/biscuit-tray.jpg" alt="Biscuits on a Tray" style="width:100%;">
+	        <div class="carousel-caption">
+	          <h3>Ingredients To Go</h3>
+	          <p>Make your own delicious treats with our new range</p>
+	        </div>
 	      </div>
 
 	      <!-- Photo by Anastasiia Ostapovych on Unsplash -->
 	      <div class="item">
 	        <img src="/Images/macrons.jpg" alt="Macrons" style="width:100%;">
+	        <div class="carousel-caption">
+	          <h3>Perfection Every Time</h3>
+	          <p>Our new Deluxe range offer the ultimate pleasure</p>
+	        </div>
 	      </div>
 	    
 	      <!-- Photo by Nerfee Mirandilla on Unsplash -->
 	      <div class="item">
 	        <img src="/Images/nescafe-cookies.jpg" alt="Cookies and Coffee" style="width:100%;">
+	        <div class="carousel-caption">
+	          <h3>Time To Relax</h3>
+	          <p>Put on the kettle and relax, after a long day</p>
+	        </div>
 	      </div>
 	    </div>
 
@@ -69,6 +82,39 @@
 	      <span class="glyphicon glyphicon-chevron-right"></span>
 	      <span class="sr-only">Next</span>
 	    </a>
+	  </div>
+	</div> <!-- End of the carousel -->
+
+
+
+	<!-- Container (About Section) -->
+	<div id="products" class="container-fluid">
+	  <div class="row">
+	    <div class="col-sm-8">
+	      <h2>Products</h2><br>
+	      
+	      <h4>We offer only the best when it comes to biscuits. Explore our diverse range today and caught some the latest offers.</h4><br>
+	      
+	      <p>From old classics to new stars, if you can think of it, then we sell it. Our range extends from main stream biscuits of all kinds, to our own fresh inhouse offerings. Not content with just that though? Well, you can also order all the ingredients you'd ever need, to make your own delicious treats today!</p><br>
+
+	      <button class="btn btn-default btn-lg" onclick="location.href='/Products/products.php">View Products</button>
+	    </div>
+	  </div>
+	</div>
+
+
+
+	<!-- Container (About Section) -->
+	<div id="our-promise" class="container-fluid">
+	  <div class="row">
+	    <div class="col-sm-8">
+	      <h2>Our Promise</h2><br>
+	      
+	      <h4>At Biscuits R' Us, it is our belief that all our customers are master bakers.. So thats why we treat you like one!</h4><br>
+	      
+	      <p>We only sell the freshest, hand picked ingredients. All our biscuits are made in Ireland to the highest quality standards. Not satisfied? We guarantee free returns and money back on all purchases that don't live up this standard.</p>
+
+	    </div>
 	  </div>
 	</div>
 
